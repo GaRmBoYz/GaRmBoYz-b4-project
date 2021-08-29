e31912483b2630626f284b86a0bcd5509d03e174
GaRmBoYz <saleela@GaRmBoYzs-Air.fios-router.home>
2021-08-29 10:01:22 -0400

    TwoNewFilesUpDated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("This is Beknazar's project");
+        System.out.println(revStr("Hello World"));
+    }
+
+    public static String revStr(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 }
