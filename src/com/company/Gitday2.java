730e2f2b337354327f182d4e19bb330bc6b3ad10
GaRmBoYz <saleela@GaRmBoYzs-Air.fios-router.home>
2021-08-29 09:42:05 -0400

    Day2ClassAdded

--- src/com/company/Gitday2.java
+++ src/com/company/Gitday2.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Gitday2 {
     public static void main(String[] args) {
-        System.out.println("Sleepy Suday");
+        System.out.println("Sleepy Sunday");
     }
 }
 
